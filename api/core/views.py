de4e3b7b079a80df1620d390e6cad134d077cd8f
Trivium0911 <triv_1993@mail.ru>
2022-08-09 11:10:42 +0300

    added pagination class

--- api/core/views.py
+++ api/core/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, pagination
 from .serializers import PostSerializer
 from .models import Post
 from rest_framework import permissions
+from rest_framework import generics
+from taggit.models import Tag
 from rest_framework.response import Response
 
 
@@ -24,3 +26,15 @@ class PostViewSet(viewsets.ModelViewSet):
     lookup_field = 'slug'
     permission_classes = [permissions.AllowAny]
     pagination_class = PageNumberSetPagination
+
+
+class TagDetailView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    pagination_class = PageNumberSetPagination
+    permission_classes = [permissions.AllowAny]
+
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug'].lower()
+        tag = Tag.objects.get(slug=tag_slug)
+        return Post.objects.filter(tags=tag)
