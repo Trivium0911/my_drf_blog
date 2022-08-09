12a7dd399d2420dd0cbacef1c559a3a7730e7fb1
Trivium0911 <triv_1993@mail.ru>
2022-08-09 09:21:19 +0300

    added pagination class

--- api/core/views.py
+++ api/core/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, pagination
 from .serializers import PostSerializer
 from .models import Post
 from rest_framework import permissions
@@ -10,3 +10,17 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     lookup_field = 'slug'
     permission_classes = [permissions.AllowAny]
+
+
+class PageNumberSetPagination(pagination.PageNumberPagination):
+    page_size = 6
+    page_size_query_param = 'page_size'
+    ordering = 'created_at'
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
+    permission_classes = [permissions.AllowAny]
+    pagination_class = PageNumberSetPagination
